1f8143d65ec2da09c2156bfa53240a222a37a1ba
Pablo Calderon <pablocalderon9408@gmail.com>
2022-04-28 23:30:49 -0500

    create abstract class and proxy

---
+++ cride/utils/models.py
@@ -0,0 +1,30 @@
+"""Django models utilities"""
+
+
+# Django
+from django.db import models
+
+class CrideModel(models.model):
+    """Comparte Ride base model.
+    CrideModel acts as an abstract base class from which every other model in the project will inherit,
+    providing created and modified fields."""
+
+    created = models.DateTimeField(
+        "created at",
+        auto_now_add=True,
+        help_text= "Date time on which the object was created"
+        )
+
+    modified = models.DateTimeField(
+        "modified at",
+        auto_now=True,
+        help_text= "Date time on which the object was modified"
+        )
+
+
+    class Meta:
+
+        abstract = True
+
+        get_latest_by = "created"
+        ordering = ["-created", "-modified"]
