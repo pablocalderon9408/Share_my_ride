7901a47513f03c86df3ec88f7db48336ccf4ac94
Pablo Calderon <pablocalderon9408@gmail.com>
2022-05-02 22:56:41 -0500

    create user app and base model

---
+++ cride/users/apps.py
@@ -0,0 +1,11 @@
+"""Users app."""
+
+# Django
+from django.apps import AppConfig
+
+
+class UsersAppConfig(AppConfig):
+    """users app config."""
+
+    name = "cride.users"
+    verbose_name = "Users"
