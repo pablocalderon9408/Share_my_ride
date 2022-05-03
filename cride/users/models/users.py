f1a112b9c3e362bef238d181387b9fa96f350a4e
Pablo Calderon <pablocalderon9408@gmail.com>
2022-05-02 23:12:39 -0500

    create model folder for user app

---
+++ cride/users/models/users.py
@@ -0,0 +1,51 @@
+"""User model."""
+
+# Django
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
+
+# Utilities
+from cride.utils.models import CrideModel
+
+
+
+class User(CrideModel, AbstractUser):
+
+
+    email = models.EmailField(
+        help_text="email address",
+        unique=True,
+        error_messages={
+            "unique": "A user with that email already exists"
+        }
+    )
+
+    phone_regex = RegexValidator(
+        regex=r'\+?1?\d{9,15}$'
+    )
+
+    phone_number = models.CharField(max_length=17, blank=True, validators=phone_regex)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username", "first_name", "last_name"]
+
+    is_client = models.BooleanField(
+        "client",
+        default=True,
+        help_text=(
+            "Help easily distinguish users and perform queries."
+            "Clients are the main type of users"),
+    )
+
+    is_verified = models.BooleanField(
+        "verified",
+        default=True,
+        help_text="Set to true when the user have verified its email address."
+    )
+
+    def __str__(self) -> str:
+        return self.username
+
+    def get_short_name(self):
+        return self.username
